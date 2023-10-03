57ea1b9c096c711cdbf9942b33f5025a75b4076c
uausofteng <uausofteng@gmail.com>
2023-10-03 00:54:33 -0700

    0x01-lock update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
+'''LockBoxes Challenge'''
+
 
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n
-    visited[0] = True
-    stack = [0]  # Start with the first box (box 0)
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if key >= 0 and key < n and not visited[key]:
-                visited[key] = True
-                stack.append(key)
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    return all(visited)
-
-# You can add more code for testing the function or other functionality here
-
-if __name__ == "__main__":
-    # Example usage:
-    boxes = [[1], [2], [3], []]
-    result = canUnlockAll(boxes)
-    print(result)
-
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
